d7b332014d7637739dc149e5916eeca7dd6bb86c
WillJolley <yeswilljolley@gmail.com>
2024-01-23 15:34:48 -0800

    add import statements, tealist function, and proptypes

---
+++ src/components/TeaList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Tea from './Tea';
+import PropTypes from 'prop-types';
+
+function TeaList(props) {
+
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.teaList.map((tea) =>
+        <Tea
+          whenTeaClicked={props.onTeaSelection}
+          name={tea.name}
+          variety={tea.variety}
+          caffeination={tea.caffeination}
+          price={tea.price}
+          id={tea.id}
+          key={tea.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+TeaList.propTypes = {
+  teaList: PropTypes.array,
+  onTeaSelection: PropTypes.fun
+};
+
+export default TeaList;
