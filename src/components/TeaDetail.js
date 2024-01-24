ff387cd55187de5bb3df121fe552c4460ab87765
WillJolley <yeswilljolley@gmail.com>
2024-01-23 16:38:35 -0800

    add import statements, tea details display, edit button, prop types

---
+++ src/components/TeaDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TeaDetail(props) {
+  const { ticket, onClickingEdit } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Tea Details</h1>
+      <h3>Name: {tea.name}</h3>
+      <h3>Variety: {tea.variety}</h3>
+      <h3>Caffeine Level: {tea.caffeination}</h3>
+      <h3>Price Per Ounce: {tea.price}</h3>
+      <button onClick={onClickingEdit}>Edit Tea</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+TeaDetail.propTypes = {
+  tea: PropTypes.object,
+  onClickingEdit: PropTypes.func
+};
+
+export default TeaDetail;
